462ccc69beee594bf62757ee64de4d75b10959e0
VictorCastao <vgcc287@gmail.com>
2019-04-30 23:35:18 -0300

    JS Tarefa 30/04
    Resolução 5 Desafios

--- respJS_001.js
+++ respJS_001.js
@@ -1,49 +1,143 @@
-console.log("Olá mundo!");
+console.log("Respostas Desafios JS_001");
 
 function Q1() {
     console.log('Q1');
     var x = Number.parseInt(document.getElementById('x').value);
     var y = Number.parseInt(document.getElementById('y').value);
-    console.log(x);
-    console.log(y);
+    console.log('Valor de X:' + x);
+    console.log('Valor de Y:' + y);
     console.log('Quociente = ' + Math.floor(x/y));
     console.log('Resto = ' + (x%y));
 }
 
 function Q2() {
     console.log('Q2');
-    var c1 = Number.parseInt(document.getElementById('c1').value);
-    var c2 = Number.parseInt(document.getElementById('c2').value);
+    var c1 = Number.parseFloat(document.getElementById('c1').value);
+    var c2 = Number.parseFloat(document.getElementById('c2').value);
     console.log('Cateto 1 = ' + c1);
     console.log('Cateto 2 = ' + c2);
     console.log('Hipotenusa = ' + Math.sqrt(Math.pow(c1,2) + Math.pow(c2,2)));
 }
+
 function Q3() {
     console.log('Q3');
     var aux = document.getElementById('profissoes');
     var profissao = aux.options[aux.selectedIndex].text;
     
-    
     console.log('Profissão Escolhida: ' + profissao);
     
-    switch(aux.options[aux.selectedIndex].value){
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-        case 'bombeiro': console.log('Substantivo: Masculino');
-        break;
-    }
+    if (profissao.endsWith('o')){console.log('Substantivo: Masculino')}
+    else{
     
+    if (profissao.endsWith('or')){console.log('Substantivo: Masculino')}
+    else{
+    
+    if (profissao.endsWith('z')){console.log('Substantivo: Feminino')}
+    else{ 
+        
+    if (profissao.endsWith('a')){console.log('Substantivo: Feminino')}
+    else{    
+        
+    console.log('Substantivo: Ambos')}
+           }
+        }
+    }
+        
+}
+
+function Q4() {
+    console.log('Q4');
+    var a_str = document.getElementById('cat1').value;
+    var b_str = document.getElementById('cat2').value;
+    var c_str = document.getElementById('hip').value;   
+
+    if (a_str && b_str && c_str){
+        console.log('Retire um valor!!')} 
+    else{
+    
+    if ((!a_str) && (!b_str) && (!c_str)){
+        console.log('Informe ao menos dois valores!!')} 
+    else{
+
+    if ((!a_str) && (!b_str)){
+        console.log('Informe mais um valor!!')} 
+    else{
+
+    if ((!a_str) && (!c_str)){
+        console.log('Informe mais um valor!!')} 
+    else{
+
+    if ((!b_str) && (!c_str)){
+        console.log('Informe mais um valor!!')} 
+    else{
+
+    if (!a_str) {
+        console.log('Lado Faltando: Cateto 1')
+        console.log('O valor do Cateto 1 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (b_str,2)))} 
+    else{    
+    
+    if (!b_str) {
+        console.log('Lado Faltando: Cateto 2')
+        console.log('O valor do Cateto 2 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (a_str,2)))} 
+    else{
+    
+    if (!c_str) {
+        console.log('Lado Faltando: Hipotenusa')
+        console.log('O valor da Hipotenusa é: ' + Math.sqrt(Math.pow (a_str,2) + Math.pow (b_str,2)))}  
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }      
 
 }
+
+function Q5(){
+        console.log('Q5');
+        var altura = Number.parseFloat(document.getElementById('altura').value);
+        var peso = Number.parseFloat(document.getElementById('peso').value);
+        var aux = document.getElementById('genero');
+        var genero = aux.options[aux.selectedIndex].value;
+        var imc = peso / Math.pow(altura,2); 
+        
+        if ((!altura) || (!peso)){
+            console.log('Informe os dois valores corretamente!!')}
+
+            console.log('O peso é: ' + peso + 'kg');
+            console.log('A altura é: ' + altura + 'm');
+            console.log('O IMC é: ' + imc);
+        
+        switch (genero){
+        case 'feminino':
+        if (imc < 18.5){console.log('Sua classificação é: Magra')}
+        else{
+        if (imc < 25.0){console.log('Sua classificação é: Sarada')}    
+        else{
+        if (imc < 30.0){console.log('Sua classificação é: Rechonchuda')}
+        else{
+        if (imc >= 30.0){console.log('Sua classificação é: Obesa')}
+                }
+            }
+        }
+        break;
+        
+        case 'masculino':
+        if (imc < 18.5){console.log('Sua classificação é: Magro')}
+        else{
+        if (imc < 25.0){console.log('Sua classificação é: Sarado')}    
+        else{
+        if (imc < 30.0){console.log('Sua classificação é: Rechonchudo')}
+        else{
+        if (imc >= 30.0){console.log('Sua classificação é: Obeso')}
+                }
+            }
+        }
+        break; 
+    }
+
+}
+
+
+
