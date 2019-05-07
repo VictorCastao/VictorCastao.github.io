d94898b4d6a20d0ffcb7253ad4b33151ac5c8642
VictorCastao <vgcc287@gmail.com>
2019-05-07 10:27:43 -0300

    Funções
    Faltando apenas criar função IMC

--- respJS_002.js
+++ respJS_002.js
@@ -17,84 +17,97 @@ function Q2() {
     console.log('Q2');
     var c1 = Number.parseFloat(document.getElementById('c1').value);
     var c2 = Number.parseFloat(document.getElementById('c2').value);
-    console.log('Cateto 1 = ' + c1);
-    console.log('Cateto 2 = ' + c2);
-    console.log('Hipotenusa = ' + Math.sqrt(Math.pow(c1,2) + Math.pow(c2,2)));
+    var resp2 = hipotenusa(c1, c2);
+    document.getElementById('RQ2').innerHTML = resp2;
+    console.log(resp2);
+
+}
+
+function hipotenusa(c1,c2){
+    return ('Cateto 1 = ' + c1 + '/' + 'Cateto 2 = ' + c2 + '/' + 'Hipotenusa = ' + Math.sqrt(Math.pow (c1, 2) + Math.pow (c2, 2)))
 }
 
 function Q3() {
     console.log('Q3');
     var aux = document.getElementById('profissoes');
     var profissao = aux.options[aux.selectedIndex].text;
+    var resp3 = profegenero(profissao);
+    document.getElementById('RQ3').innerHTML = resp3;
+    console.log(resp3);
+}
+
+
+function profegenero(profissao){
     
-    console.log('Profissão Escolhida: ' + profissao);
-    
-    if (profissao.endsWith('o')){console.log('Substantivo: Masculino')}
+    if (profissao.endsWith('o')){return('Profissão Escolhida: ' + profissao + '/' + 'Substantivo: Masculino')}
     else{
     
-    if (profissao.endsWith('or')){console.log('Substantivo: Masculino')}
+    if (profissao.endsWith('or')){return('Profissão Escolhida: ' + profissao + '/' + 'Substantivo: Masculino')}
     else{
     
-    if (profissao.endsWith('z')){console.log('Substantivo: Feminino')}
+    if (profissao.endsWith('z')){return('Profissão Escolhida: ' + profissao + '/' + 'Substantivo: Feminino')}
     else{ 
         
-    if (profissao.endsWith('a')){console.log('Substantivo: Feminino')}
+    if (profissao.endsWith('a')){return('Profissão Escolhida: ' + profissao + '/' + 'Substantivo: Feminino')}
     else{    
         
-    console.log('Substantivo: Ambos')}
-           }
+    return('Profissão Escolhida: ' + profissao + '/' + 'Substantivo: Ambos')
+                }    
+            }
         }
-    }
-        
+    }    
 }
+    
 
 function Q4() {
     console.log('Q4');
     var a_str = document.getElementById('cat1').value;
     var b_str = document.getElementById('cat2').value;
     var c_str = document.getElementById('hip').value;   
+    var resp4 = faltando(a_str,b_str,c_str);
+    document.getElementById('RQ4').innerHTML = resp4;
+    console.log(resp4);
+}
+
+function faltando(a_str,b_str,c_str){
 
     if (a_str && b_str && c_str){
-        console.log('Retire um valor!!')} 
+        return('Retire um valor!!')} 
     else{
     
     if ((!a_str) && (!b_str) && (!c_str)){
-        console.log('Informe ao menos dois valores!!')} 
+        return('Informe ao menos dois valores!!')} 
     else{
 
     if ((!a_str) && (!b_str)){
-        console.log('Informe mais um valor!!')} 
+        return('Informe mais um valor!!')} 
     else{
 
     if ((!a_str) && (!c_str)){
-        console.log('Informe mais um valor!!')} 
+        return('Informe mais um valor!!')} 
     else{
 
     if ((!b_str) && (!c_str)){
-        console.log('Informe mais um valor!!')} 
+        return('Informe mais um valor!!')} 
     else{
 
     if (!a_str) {
-        console.log('Lado Faltando: Cateto 1')
-        console.log('O valor do Cateto 1 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (b_str,2)))} 
+        return('Lado Faltando: Cateto 1' + '/' + 'O valor do Cateto 1 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (b_str,2)))}
     else{    
     
     if (!b_str) {
-        console.log('Lado Faltando: Cateto 2')
-        console.log('O valor do Cateto 2 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (a_str,2)))} 
+        return('Lado Faltando: Cateto 2' + '/' + 'O valor do Cateto 2 é: ' + Math.sqrt(Math.pow (c_str,2) - Math.pow (a_str,2)))}
     else{
     
     if (!c_str) {
-        console.log('Lado Faltando: Hipotenusa')
-        console.log('O valor da Hipotenusa é: ' + Math.sqrt(Math.pow (a_str,2) + Math.pow (b_str,2)))}  
-                                }
+        return('Lado Faltando: Hipotenusa' + '/' + 'O valor da Hipotenusa é: ' + Math.sqrt(Math.pow (a_str,2) + Math.pow (b_str,2)))}
                             }
                         }
                     }
                 }
             }
-        }      
-
+        }
+    }      
 }
 
 function Q5(){
@@ -103,37 +116,46 @@ function Q5(){
         var peso = Number.parseFloat(document.getElementById('peso').value);
         var aux = document.getElementById('genero');
         var genero = aux.options[aux.selectedIndex].value;
-        var imc = peso / Math.pow(altura,2); 
-        
-        if ((!altura) || (!peso)){
-            console.log('Informe os dois valores corretamente!!')}
+        var imc = peso / Math.pow(altura,2);
+        var resp5 = calculo (altura,peso,genero,imc);
+        document.getElementById('RQ5').innerHTML = resp5;
+        console.log(resp5);
+}
 
-            console.log('O peso é: ' + peso + 'kg');
-            console.log('A altura é: ' + altura + 'm');
-            console.log('O IMC é: ' + imc);
+function calculo(altura,peso,genero,imc){   
+        if ((!altura) || (!peso)){
+            return('Informe os dois valores corretamente!!')}
+
+            return('O peso é: ' + peso + 'kg' + '/' + 
+                   'A altura é: ' + altura + 'm' + '/' +
+                   'O IMC é: ' + imc + '/' +
+                   'Sua classificação é: ' + genclass(genero,imc))
+}
+            
+function genclass(genero,imc){        
         
-        switch (genero){
+    switch (genero){
         case 'feminino':
-        if (imc < 18.5){console.log('Sua classificação é: Magra')}
+        if (imc < 18.5){return('Magra')}
         else{
-        if (imc < 25.0){console.log('Sua classificação é: Sarada')}    
+        if (imc < 25.0){return ('Sarada')}    
         else{
-        if (imc < 30.0){console.log('Sua classificação é: Rechonchuda')}
+        if (imc < 30.0){return ('Rechonchuda')}
         else{
-        if (imc >= 30.0){console.log('Sua classificação é: Obesa')}
+        if (imc >= 30.0){return('Obesa')}
                 }
             }
         }
         break;
         
         case 'masculino':
-        if (imc < 18.5){console.log('Sua classificação é: Magro')}
+        if (imc < 18.5){return('Magro')}
         else{
-        if (imc < 25.0){console.log('Sua classificação é: Sarado')}    
+        if (imc < 25.0){return ('Sarado')}    
         else{
-        if (imc < 30.0){console.log('Sua classificação é: Rechonchudo')}
+        if (imc < 30.0){return ('Rechonchudo')}
         else{
-        if (imc >= 30.0){console.log('Sua classificação é: Obeso')}
+        if (imc >= 30.0){return('Obeso')}
                 }
             }
         }
