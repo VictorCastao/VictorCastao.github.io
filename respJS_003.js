be7b32eb4f59bfdce37b8f6e9a6fe4e548900e5a
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-05-23 11:24:03 -0300

    Exercicios JS3

--- respJS_003.js
+++ respJS_003.js
@@ -36,22 +36,66 @@ function Q2() {
 
 
 
+var listaStrings = [];
 
-function Q3(listaStrings) {
-    var listaStrings = [];
-    var novaString = document.getElementById('string').value;
-    console.log('Q3');
-    listaStrings.push(novaString);
+function acrescentar() {
+    var aux = document.getElementById('novaString').value;
+    listaStrings.push(aux);
+    document.getElementById("novaString").value = "";
+    document.getElementById("nova").innerHTML = listaStrings;
     console.log(listaStrings);
-    document.getElementById("string").value = "";
-    document.getElementById("RQ3").innerHTML = listaStrings;
-    document.getElementById("RQ3-1").innerHTML = listaStrings[0].toUppercase;
-
 }
 
-function MAIUSCULA(listaStrings){
-    for (i = 0; i < listaStrings.length; i++){
-        console.log(listaStrings[i]);
+function Q3(){}
+
+
+function QuickSort_(X, IniVet, FimVet){
+    var i = 0;
+    var j = X.length - 1;
+    var pivo = X[Math.floor((i+j)/2)];
+    var aux;
+
+   while(i <= j){
+     while (X[i] < pivo){ 
+              i++;
+           }
+          
+          while (X[j] > pivo) {
+              j--;
+          }
+          if (i <= j) {
+              aux  = X[i];
+              X[i] = X[j];
+              X[j] = aux;
+              i++;
+              j--;
+        }
+    }          
+   if (IniVet < j) {
+      QuickSort(X, IniVet, j);
     }
+   if (i < FimVet) {
+      QuickSort(X, i, FimVet);
+   }
+}
+
+function QuickSort(X){
+    var Y = Array.from(X);
+    QuickSort_(Y, 0, Y.length-1);
+    return Y;
+}
+
+
+function Q4(){
+    console.log("Q4");
+    var aux = document.getElementById('numeroQ4').value;
+    var numerosQ4 = aux.split(';');
+    var ordenados;
+    for (let i=0;i<numerosQ4.length;i++){
+        numerosQ4[i] = Number.parseFloat(numerosQ4[i]);
+    }
+    ordenados=QuickSort(numerosQ4);
+    console.log(ordenados);
+    document.getElementById("RQ4").innerHTML=ordenados;
 }
 
