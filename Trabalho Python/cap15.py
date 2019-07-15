7c075d6dc208b57b86c1352c9edd3bfcc239e22b
VictorCastao <vgcc287@gmail.com>
2019-07-15 14:23:24 -0300

    correção de erros
    Retirando arquivos da pasta para corrigir erro com o repositório

--- Trabalho Python/cap15.py
+++
@@ -1,5 +0,0 @@
-import datetime, time	
-desperta = datetime.datetime(2019,7,9,21,23,0)
-while datetime.datetime.now() < desperta:
-    time.sleep(1)
-print('Cordei')
--- Trabalho Python/jsonteste.py
+++
@@ -1,4 +0,0 @@
-pythonValue	=	{'isCat':	True,	'miceCaught':	0,	'name':	'Zophie','felineIQ':	None} 
-import	json 
-stringOfJsonData	=	json.dumps(pythonValue) 
-print(stringOfJsonData) 
